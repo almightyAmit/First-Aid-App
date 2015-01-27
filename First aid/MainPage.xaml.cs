e4c70d1eb4837369b03f67f3ab536135d42b88c4
amitsaroj001 <amitsaroj2012@gmail.com>
2015-01-28 04:26:44 +0530

    First Aid Windows Phone App

---
+++ First aid/MainPage.xaml.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using First_aid.Resources;
+using Microsoft.Phone.Tasks;
+using System.IO;
+using System.IO.IsolatedStorage;
+
+
+namespace First_aid
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            bool load_def = false;
+            InitializeComponent();
+            String str = "0";
+            int num;
+            bool stat = false;
+            IsolatedStorageFile myspace = IsolatedStorageFile.GetUserStoreForApplication();
+            String tem = "a";
+            try
+            {
+                using (var isoFileStream = new IsolatedStorageFileStream("number.txt", FileMode.Open, myspace))
+                {
+                    using (var isoFileReader = new StreamReader(isoFileStream))
+                    {
+                        str = isoFileReader.ReadToEnd();
+                        String[] temp = str.Split('.');
+                        num = Convert.ToInt32(temp[0]);
+                        if (temp[1].Equals("1"))
+                            stat = true;
+                        tem = temp[1];
+                    }
+                }
+
+            }
+            catch
+            {
+                num = 0;
+            }
+            if (num == 0)
+                load_def = true;
+            if (!stat)
+                if (num % 5 == 0 && num != 0)
+                {
+                    MessageBoxResult res = MessageBox.Show("You have used First aid " + num + " times. Rate and Review us to help us improve.", "First aid", MessageBoxButton.OKCancel);
+                    if (res == MessageBoxResult.OK)
+                    {
+                        MarketplaceReviewTask marketplaceReviewTask = new MarketplaceReviewTask();
+                        marketplaceReviewTask.Show();
+                        stat = true;
+                    }
+                    else if (res == MessageBoxResult.Cancel && num == 5)
+                    {
+                        MessageBoxResult res1 = MessageBox.Show("Do you want to send feedback?", "First aid", MessageBoxButton.OKCancel);
+                        if (res1 == MessageBoxResult.OK)
+                        {
+                            EmailComposeTask emailTask = new EmailComposeTask();
+                            emailTask.Subject = "Feedback for app: First aid";
+                            emailTask.To = "alok1994@outlook.com";
+                            emailTask.Show();
+                        }
+                    }
+
+
+                }
+            num++;
+            String write;
+            if (stat)
+                write = num + ".1";
+            else
+                write = num + ".0";
+            using (var isoFileStream = new IsolatedStorageFileStream("number.txt", FileMode.OpenOrCreate, myspace))
+            {
+                using (var isoFileWriter = new StreamWriter(isoFileStream))
+                {
+                    isoFileWriter.WriteLine(write);
+                }
+            }
+
+            // Sample code to localize the ApplicationBar
+            //BuildLocalizedApplicationBar();
+        }
+
+        private void Button_Tap(object sender, System.Windows.Input.GestureEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/aids.xaml", UriKind.Relative));
+        }
+
+        // Sample code for building a localized ApplicationBar
+        //private void BuildLocalizedApplicationBar()
+        //{
+        //    // Set the page's ApplicationBar to a new instance of ApplicationBar.
+        //    ApplicationBar = new ApplicationBar();
+
+        //    // Create a new button and set the text value to the localized string from AppResources.
+        //    ApplicationBarIconButton appBarButton = new ApplicationBarIconButton(new Uri("/Assets/AppBar/appbar.add.rest.png", UriKind.Relative));
+        //    appBarButton.Text = AppResources.AppBarButtonText;
+        //    ApplicationBar.Buttons.Add(appBarButton);
+
+        //    // Create a new menu item with the localized string from AppResources.
+        //    ApplicationBarMenuItem appBarMenuItem = new ApplicationBarMenuItem(AppResources.AppBarMenuItemText);
+        //    ApplicationBar.MenuItems.Add(appBarMenuItem);
+        //}
+    }
+}
---
+++ First aid/aids.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace First_aid
+{
+    public partial class aids : PhoneApplicationPage
+    {
+        public aids()
+        {
+            InitializeComponent();
+        }
+
+        private void tx1_Tap(object sender, System.Windows.Input.GestureEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/Page1.xaml", UriKind.Relative));
+        }
+
+        private void tx01_Tap(object sender, System.Windows.Input.GestureEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/Page2.xaml", UriKind.Relative));
+        }
+
+        private void tx2_Tap(object sender, System.Windows.Input.GestureEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/Page3.xaml", UriKind.Relative));
+        }
+
+        private void tx3_Tap(object sender, System.Windows.Input.GestureEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/Page4.xaml", UriKind.Relative));
+        }
+
+        private void tx5_Tap(object sender, System.Windows.Input.GestureEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/Page5.xaml", UriKind.Relative));
+        }
+
+        private void tx6_Tap(object sender, System.Windows.Input.GestureEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/Page6.xaml", UriKind.Relative));
+        }
+
+        private void tx7_Tap_1(object sender, System.Windows.Input.GestureEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/Page7.xaml", UriKind.Relative));
+        }
+
+        private void tx7_Tap(object sender, System.Windows.Input.GestureEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/Page8.xaml", UriKind.Relative));
+        }
+
+        private void tx02_Tap(object sender, System.Windows.Input.GestureEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/Page9.xaml", UriKind.Relative));
+        }
+
+        private void tx03_Tap(object sender, System.Windows.Input.GestureEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/Page10.xaml", UriKind.Relative));
+        }
+
+        private void tx8_Tap(object sender, System.Windows.Input.GestureEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/Page11.xaml", UriKind.Relative));
+        }
+
+        private void tx4_Tap(object sender, System.Windows.Input.GestureEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/Page12.xaml", UriKind.Relative));
+        }
+
+        private void tx9_Tap(object sender, System.Windows.Input.GestureEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/Page13.xaml", UriKind.Relative));
+        }
+
+        private void tx10_Tap(object sender, System.Windows.Input.GestureEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/Page14.xaml", UriKind.Relative));
+        }
+    }
+}
